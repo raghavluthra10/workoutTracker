0add3b0fa57ed79188a6dfc0bfb5080b76a9d003
raghav786 <58934695+raghav786@users.noreply.github.com>
2021-05-03 18:19:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Login';
+import SignUp from './SignUp';
+import Home from './Home';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="App">
+      <Switch>
+        <Route path='/' exact> <Home  /> </Route>
+        <Route path='/login' exact> <Login  /> </Route>
+        <Route path='/signup' exact> <SignUp  /> </Route>
+        <Route path='/dashboard' exact> <Dashboard  /> </Route>
+        
+      </Switch>
     </div>
+    </Router>
+    
   );
 }
 
---
+++ src/Login.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Login.css';
+import Button from '@material-ui/core/Button';
+
+
+const Login = () => {
+
+    const login = () => {
+      
+    }
+
+    
+    return (
+        <div className='login'>
+            <div className='login__header'>
+                <h1>
+                    Login
+                </h1>
+            </div>
+            <form className='login__form'>
+                <label> Email: </label>
+                <input type='text' />
+
+                <label> Password: </label>
+                <input type='password' />
+
+                <Button variant='outlined' onClick={login} type='submit' > Login </Button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
